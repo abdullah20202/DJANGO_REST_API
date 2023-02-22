30ceebb9877f3fccc3478a7d1a7af087ec2bc55d
abdullah20202 <abdullah.munaibari@gmail.com>
2023-02-22 04:43:59 +0300

    Added custom user profile model manager and migration

--- restAPI/settings.py
+++ restAPI/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'restAPI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
