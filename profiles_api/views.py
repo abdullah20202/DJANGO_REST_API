ce7be5b1eed0018ad6baf0521a6865fd9f32005c
abdullah20202 <abdullah.munaibari@gmail.com>
2023-02-27 13:20:30 +0300

    Adedd seraching to user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,8 @@ from profiles_api import serializers
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
+from rest_framework import filters
+
 
 class HelloApiView(APIView):
 
@@ -113,3 +115,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
